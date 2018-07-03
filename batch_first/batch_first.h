4a253407a18988aaff3194ec8bb527eeb6201434
MaxRogovin <40808497+MaxRogovin@users.noreply.github.com>
2018-07-03 14:43:06 -0700

    Create batch_first.h

---
+++ batch_first/batch_first.h
@@ -0,0 +1,12 @@
+#ifndef BATCH_FIRST_H
+#define BATCH_FIRST_H
+
+#include "batch_first_numba_output.h" //this should be the C file that numba spits out
+
+//KHASH_MAP_INIT_INT64(i2i, khint64_t)  not sure what this is for, leaving it in in case needed to modify later
+
+static inline void *Batch_first_function(void) {
+    return 0;
+}
+
+#endif
